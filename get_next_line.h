e662f13755321a2eb8aa628e0ae75e5da95a4005
Maelyss ARNAUD <marnaud@e1r1p1.42.fr>
2017-01-04 18:41:00 +0100

    yo les amis c est squeezie

--- get_next_line.h
+++ get_next_line.h
@@ -2,14 +2,14 @@
 
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 8385223
 
 typedef struct	s_fd
 {
-	char		*reste;
-	char		buffer[BUFF_SIZE];
+	char		*old;
 	int		fd;
 	struct s_fd	*next;
+	struct s_fd *previous;
 }		t_fd;
 
 # include <fcntl.h>
