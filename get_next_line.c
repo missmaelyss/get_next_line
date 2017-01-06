0a035416c65442acb6f72bc39bb2de7a297321dc
Maelyss ARNAUD <marnaud@e1r3p9.42.fr>
2017-01-06 16:56:50 +0100

    yo

--- get_next_line.c
+++ get_next_line.c
@@ -1,173 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marnaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/06 13:48:06 by marnaud           #+#    #+#             */
+/*   Updated: 2017/01/06 16:51:41 by marnaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include <stdio.h>
 
-/*int	get_next_line(const int fd, char **line)
-  {
-  char	buffer[10];
-  int	n;
-  int	oui;
-  char	*tmp;
-  char	*tmp2;
-  size_t	size;
-
-  oui = 0;
-  while (oui != 1)
-  {
-  n = 0;
-  while (tmp[n] != '\n' && tmp[n] != '\0')
-  {
-  n++;
-  if (tmp[n] == '\n')
-  oui = 1;
-  }
-  read(fd, buffer, 10);
-  n = 0;
-  size = 0;
-  while (buffer[n] != '\n' && n < 10)
-  {
-  if (buffer[n + 1] == '\n')
-  oui = 1;
-  size++;
-  n++;
-  }
-  tmp2 = ft_strnew(size);
-  tmp2 = ft_strncat(tmp2, buffer, size);
-  tmp = ft_strjoin((char const *)tmp, (char const *)tmp2);
-//		printf("%s", tmp);
-}
-printf("%s\n", tmp);
- *line = tmp;
- return (2);
- }
- */
 t_fd	*gnl_new_fd(int fd)
 {
 	t_fd	*lst;
 
 	lst = (t_fd *)malloc(sizeof(t_fd) * 1);
-	(*lst).reste = NULL;
+	(*lst).old = NULL;
 	(*lst).fd = fd;
 	(*lst).next = NULL;
+	(*lst).previous = NULL;
 	return (lst);
 }
 
-int	get_next_line(const int fd, char **line)
+void	fonction_1(char **line, t_fd *lst, const char *s, int i)
+{
+	size_t	n;
+	char	*tmp;
+
+	n = 0;
+	while (s[n] != '\n' && s[n])
+		n++;
+	if (i == 1)
+		*line = ft_strjoin(lst->old, ft_strsub(s, 0, n));
+	else
+		*line = ft_strsub(s, 0, n);
+	tmp = lst->old;
+	lst->old = ft_strdup(&(s[n + 1]));
+}
+
+int		condition(t_fd *lst, int fd, char **line)
+{
+	char	buffer[BUFF_SIZE + 1];
+	int		n;
+
+	n = read(fd, buffer, BUFF_SIZE);
+	if (n > 0)
+	{
+		buffer[n] = '\0';
+		if (ft_strchr((const char *)buffer, '\n'))
+			fonction_1(line, lst, buffer, 1);
+		else
+		{
+			lst->old = ft_strjoin(lst->old, buffer);
+			get_next_line(fd, line);
+		}
+	}
+	else if (n == 0)
+	{
+		if (lst->old != NULL)
+			fonction_1(line, lst, lst->old, 3);
+		return (0);
+	}
+	else
+		return (-1);
+	return (1);
+}
+
+int		get_next_line(const int fd, char **line)
 {
 	static t_fd	*lst = NULL;
-	int		n;
-	int		i;
 
+	if (fd < 0 || line == NULL)
+		return (-1);
 	if (lst == NULL)
 		lst = gnl_new_fd(fd);
 	else
 	{
-		while ((*lst).fd != fd && (*lst).next != NULL)
-			lst++;
+		while (lst->previous != NULL)
+			lst = lst->previous;
+		while ((*lst).fd != fd && (*lst).next)
+			lst = lst->next;
 		if ((*lst).fd != fd)
 		{
 			(*lst).next = gnl_new_fd(fd);
-			*lst = (*lst).next
-		}
-
-	}
-	while (ft_atoi(sto[p][0]) != fd && )
-	{
-
-	}
-	*line = (*lst).reste;
-	read(fd, buffer, BUFF_SIZE);
-	n = 0;
-	while(buffer[n] != '\n' && buffer[n] != '\0')
-	{
-		n++;
-		if (buffer[n] == '\0')
-		{
-			*line = ft_strjoin(*line, ft_strsub(buffer, 0, n - 1));
-			read(fd, buffer, BUFF_SIZE);
-			n = 0;
-		}
-		if (buffer[n] == '\n' && buffer[n + 1] != '\0')
-		{
-			i = n + 1;
-			while (buffer[i] != '\0')
-				i++;
-			(*lst).reste = ft_strsub(buffer, n + 1, (i - 1) - (n + 1));
+			(*lst).next->previous = lst;
+			lst = (*lst).next;
 		}
 	}
-	return (1);
-}
-	/*{
-	char	buffer[BUFF_SIZE];
-	size_t	size;
-	size_t	n;
-
-	char		*tmp;
-//	char		*line;
-	static char	*reste = NULL;
-
-	n = 0;
-	if (reste && reste[n + 1] != 0)
+	if (lst->old != NULL && ft_strchr((const char *)lst->old, '\n'))
 	{
-		while (reste[n] != '\n' && reste[n] != '\0')
-			n++;
-		*line = ft_strnew(n);
-		*line = ft_strncat(*line, reste, n);
-		reste = ft_strsub(reste, n + 1, BUFF_SIZE - (n + 1));
-//		printf("line = %s\nreste = %s\n", line, reste);	
+		fonction_1(line, lst, lst->old, 2);
+		return (1);
 	}
-	else
-	{
-		*line = ft_strjoin(reste, NULL);
-
-		read(fd, buffer, BUFF_SIZE);
-
-		printf("buffer = %s\nreste = %s\n", buffer, reste);
-
-		size = 0;
-		n = 0;	
-		while (buffer[n] != '\n' && n < BUFF_SIZE && buffer[n] != '\0')
-		{
-			size++;
-			n++;
-			if (n == BUFF_SIZE)
-			{
-				tmp = ft_strnew(size);
-				tmp = ft_strncat(tmp, buffer, size);
-				*line = ft_strjoin(*line, tmp);
-				read(fd, buffer, BUFF_SIZE);
-				size = 0;
-				n = 0;
-			}
-		}
-
-		tmp = ft_strnew(size);
-		tmp = ft_strncat(tmp, buffer, size);
-
-		*line = ft_strjoin(*line, tmp);
-
-		if (size + 1 < BUFF_SIZE)
-			reste = ft_strsub(buffer, size + 1, BUFF_SIZE - (size + 1));
-//		printf("size = %d\ntmp = %s\nline = %s\nreste = %s\n", size, tmp, line, reste);	
-	}
-	return (1);
-}*/
-
-int	main(int ac, char **av)
-{
-	int 	fd;
-	int	fd2;
-	char		**line;
-
-	if (ac == 1)
-		return (0);
-	fd = open(av[1], O_RDONLY);
-	get_next_line((const int)fd, line);
-	printf("line : %s\n", *line);
-	get_next_line((const int)fd, line);
-	printf("line : %s\n", *line);
-	get_next_line((const int)fd, line);
-	printf("line : %s\n", *line);
-	get_next_line((const int)fd, line);
-	printf("line : %s\n", *line);
-	return (0);
+	return (condition(lst, fd, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marnaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/06 16:39:29 by marnaud           #+#    #+#             */
+/*   Updated: 2017/01/06 16:40:02 by marnaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 8385223
+# define BUFF_SIZE 1000
+
+# include <fcntl.h>
+# include "libft/libft.h"
 
 typedef struct	s_fd
 {
 	char		*old;
-	int		fd;
+	int			fd;
 	struct s_fd	*next;
 	struct s_fd *previous;
-}		t_fd;
+}				t_fd;
 
-# include <fcntl.h>
-# include "libft/libft.h"
+int				get_next_line(const int fd, char **line);
 
 #endif
